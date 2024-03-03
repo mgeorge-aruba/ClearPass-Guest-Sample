45fe5178b073445f3efdec68484c2d05fdad9f5b
mgeorge-aruba <105308214+mgeorge-aruba@users.noreply.github.com>
2024-03-03 08:33:29 -0800

    Update credsConfig.py

--- credsConfig.py
+++ credsConfig.py
@@ -1,5 +1,5 @@
 
 #This is a sample credentials file. Original file should be imported in createNAD.py script
 
-clientid = "Client1"
-clientsecret = "n6fnNsi07lK+MKC3Taox+p/Lbf4mgDc9JfSU3rLp+sTJ"
+clientid = "API Client"
+clientsecret = "xsdshjdshj-SAMPLE-Secret-p/Lbf4mgDc9JfSU3rLp+sTJ"
